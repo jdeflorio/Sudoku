ed5709d70ca04c6b62a96fdb69911ca5eb83a17c
jdeflorio <joedeflorio22@gmail.com>
2021-01-16 20:07:38 -0500

    Completed game and auto-solver

--- Sudoku.py
+++ Sudoku.py
@@ -30,7 +30,7 @@ def main():
              [3, 4, 5, 2, 8, 6, 1, 7, 9]
              ]
     """
-    grid = createGrid("grid1.txt")
+    grid = createGrid("grid3.txt")
 
     drawBoxNums(screen, grid)
 
@@ -54,27 +54,29 @@ def main():
                     else:
                         Tk().wm_withdraw()
                         messagebox.showinfo('Checker','Wrong')
+                if event.key == pygame.K_s:
+                    solveGrid(grid, screen)
             if (event.type == pygame.KEYDOWN) and canChangeBox:
                 if event.key == pygame.K_1 or event.key == pygame.K_KP1:
-                    updateGrid(mousex, mousey, 1, grid, screen)
+                    updateGrid(grid, screen, mousex, mousey, 1)
                 if event.key == pygame.K_2 or event.key == pygame.K_KP2:
-                    updateGrid(mousex, mousey, 2, grid, screen)
+                    updateGrid(grid, screen, mousex, mousey, 2)
                 if event.key == pygame.K_3 or event.key == pygame.K_KP3:
-                    updateGrid(mousex, mousey, 3, grid, screen)
+                    updateGrid(grid, screen, mousex, mousey, 3)
                 if event.key == pygame.K_4 or event.key == pygame.K_KP4:
-                    updateGrid(mousex, mousey, 4, grid, screen)
+                    updateGrid(grid, screen, mousex, mousey, 4)
                 if event.key == pygame.K_5 or event.key == pygame.K_KP5:
-                    updateGrid(mousex, mousey, 5, grid, screen)
+                    updateGrid(grid, screen, mousex, mousey, 5)
                 if event.key == pygame.K_6 or event.key == pygame.K_KP6:
-                    updateGrid(mousex, mousey, 6, grid, screen)
+                    updateGrid(grid, screen, mousex, mousey, 6)
                 if event.key == pygame.K_7 or event.key == pygame.K_KP7:
-                    updateGrid(mousex, mousey, 7, grid, screen)
+                    updateGrid(grid, screen, mousex, mousey, 7)
                 if event.key == pygame.K_8 or event.key == pygame.K_KP8:
-                    updateGrid(mousex, mousey, 8, grid, screen)
+                    updateGrid(grid, screen, mousex, mousey, 8)
                 if event.key == pygame.K_9 or event.key == pygame.K_KP9:
-                    updateGrid(mousex, mousey, 9, grid, screen)
+                    updateGrid(grid, screen, mousex, mousey, 9)
                 if event.key == pygame.K_BACKSPACE or event.key == pygame.K_DELETE:
-                    updateGrid(mousex, mousey, 0, grid, screen)
+                    updateGrid(grid, screen, mousex, mousey, 0)
                 canChangeBox = False
 
 
@@ -101,8 +103,47 @@ def createGrid(file):
         row = row + 1
     return grid
 
+def drawBoxNums(gameScreen, gameGrid):
+    font = pygame.font.Font('freesansbold.ttf', 120)
+    y = 0
+    for row in range(len(gameGrid)):
+        column = 0
+        for i in range(0, SCREENWIDTH, CELLSIZE):
+            if gameGrid[row][column] > 0:
+                text = font.render(str(gameGrid[row][column]), True, BLACK)
+                textRect = text.get_rect()
+                textRect.x = i + 15
+                textRect.y = y
+                blit = gameScreen.blit(text, textRect)
+            column = column + 1
+        y = y + CELLSIZE
+
+def drawGridLines(gameScreen):
+    #draws cell lines in light gray
+    for i in range(0, SCREENWIDTH, CELLSIZE):
+        pygame.draw.line(gameScreen, LIGHTGRAY, (i,0), (i, SCREENHEIGHT))
+    for i in range(0, SCREENHEIGHT, CELLSIZE):
+        pygame.draw.line(gameScreen, LIGHTGRAY, (0,i), (SCREENWIDTH, i))
+    #draws box lines in black
+    for i in range(0, SCREENWIDTH, BOXSIZE):
+        pygame.draw.line(gameScreen, BLACK, (i,0), (i, SCREENHEIGHT))
+    for i in range(0, SCREENHEIGHT, BOXSIZE):
+        pygame.draw.line(gameScreen, BLACK, (0,i), (SCREENWIDTH, i))
 
 
+
+
+
+def updateGrid(gameGrid, screen, xpos=-1, ypos=-1, value=-1):
+    if xpos != -1 and ypos != -1:
+        #find pos on grid
+        row = int(ypos/CELLSIZE)
+        column = int(xpos/CELLSIZE)
+        gameGrid[row][column] = value
+    screen.fill(WHITE)
+    drawGridLines(screen)
+    drawBoxNums(screen, gameGrid)
+    pygame.display.update()
 
 def isGridCorrect(grid):
     #check horizontal
@@ -125,46 +166,49 @@ def isGridCorrect(grid):
                 return False
     return True
 
-def updateGrid(xpos, ypos, value, gameGrid, screen):
-    #find pos on grid
-    row = int(ypos/CELLSIZE)
-    column = int(xpos/CELLSIZE)
-    gameGrid[row][column] = value
-    screen.fill(WHITE)
-    drawGridLines(screen)
-    drawBoxNums(screen, gameGrid)
-    pygame.display.update()
 
-
-def drawBoxNums(gameScreen, gameGrid):
-    font = pygame.font.Font('freesansbold.ttf', 120)
-    y = 0
+def findEmpty(gameGrid):
     for row in range(len(gameGrid)):
-        column = 0
-        for i in range(0, SCREENWIDTH, CELLSIZE):
-            if gameGrid[row][column] > 0:
-                text = font.render(str(gameGrid[row][column]), True, BLACK)
-                textRect = text.get_rect()
-                textRect.x = i + 15
-                textRect.y = y
-                blit = gameScreen.blit(text, textRect)
-            column = column + 1
-        y = y + CELLSIZE
+        for column in range(len(gameGrid[row])):
+            if gameGrid[row][column] == 0:
+                return (row, column)
+    return (-1, -1)
+                    
 
+def isValid(gameGrid, square, num):
+    for i in range(len(gameGrid)):
+        if gameGrid[square[0]][i] == num and i != square[1]:
+            return False
+    
+    for i in range(len(gameGrid)):
+        if gameGrid[i][square[1]] == num and i != square[0]:
+            return False
 
+    boxRow = square[0] // 3
+    boxColumn = square[1] // 3
+    
+    for i in range(boxRow*3, boxRow*3 + 3):
+        for j in range(boxColumn*3, boxColumn*3 + 3):
+            if gameGrid[i][j] == num and (i, j) != square:
+                return False
 
-def drawGridLines(gameScreen):
-    #draws cell lines in light gray
-    for i in range(0, SCREENWIDTH, CELLSIZE):
-        pygame.draw.line(gameScreen, LIGHTGRAY, (i,0), (i, SCREENHEIGHT))
-    for i in range(0, SCREENHEIGHT, CELLSIZE):
-        pygame.draw.line(gameScreen, LIGHTGRAY, (0,i), (SCREENWIDTH, i))
-    #draws box lines in black
-    for i in range(0, SCREENWIDTH, BOXSIZE):
-        pygame.draw.line(gameScreen, BLACK, (i,0), (i, SCREENHEIGHT))
-    for i in range(0, SCREENHEIGHT, BOXSIZE):
-        pygame.draw.line(gameScreen, BLACK, (0,i), (SCREENWIDTH, i))
-
+    return True
+        
+def solveGrid(gameGrid, screen):
+    emptySquare = findEmpty(gameGrid) #returns (row, column) of empty square
+    if emptySquare == (-1, -1):
+        return True             #check if grid is full
+    else:
+        for i in range(1, 10):
+            if isValid(gameGrid, emptySquare, i):       #see if a number can fit in square 
+                gameGrid[emptySquare[0]][emptySquare[1]] = i
+                updateGrid(gameGrid, screen)
+                if solveGrid(gameGrid, screen):         #recursivly check new grid, if new grid is full then returns true
+                    return True
+                else:                                   #if new check iterates thorugh 1-9 and does not find a value that fits, replaces square with zero and returns false to backtrack
+                    gameGrid[emptySquare[0]][emptySquare[1]] = 0
+                    updateGrid(gameGrid, screen)
+    return False                    
 
 
 main()
